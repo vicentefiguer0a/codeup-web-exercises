35e24b37f19ab32fef4aa91fc4410c3b65af0bb3
Vicente Figueroa <figueroa.vicente9@gmail.com>
2022-09-02 15:18:53 -0500

    saving progress in jquery selectors exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,5 +1,12 @@
 "use strict";
 
-$(function() {
-    alert("DOM has finished loading.");
-});
+// $(function() {
+//     alert("DOM has finished loading.");
+// });
+
+const mainHeading = $("#main-heading").html();
+// alert(mainHeading);
+
+const subArticle = $("#sub-article").html();
+alert(subArticle);
+
