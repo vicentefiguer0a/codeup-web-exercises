98903746bfb4a76f985f900702c3cce855725c9d
Vicente Figueroa <figueroa.vicente9@gmail.com>
2022-09-02 15:43:26 -0500

    jQuery element selectors exercise portion complete

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -10,5 +10,14 @@
 // const subArticle = $("#sub-article").html();
 // alert(subArticle);
 
-$(".codeup").css("border", "solid 1px red");
+// $(".codeup").css("border", "solid 1px red");
 
+$("li").css("font-size", "20px");
+
+const myH1 = $("h1").html();
+$("p").html();
+$("li").html();
+
+alert(myH1);
+
+$("h1, p, li");
