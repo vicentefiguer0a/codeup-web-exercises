acc4c48777d6c43d3d894eb6b26ff56499787f98
Vicente Figueroa <figueroa.vicente9@gmail.com>
2022-09-06 09:58:54 -0500

    jQuery selectors exercise reviewed and completed to satisfaction

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,24 +1,25 @@
 "use strict";
-
+// jQUERY INTRODUCTION EXERCISE BELOW
 // $(function() {
 //     alert("DOM has finished loading.");
 // });
 
-// const mainHeading = $("#main-heading").html();
-// alert(mainHeading);
 
-// const subArticle = $("#sub-article").html();
-// alert(subArticle);
+// jQUERY SELECTORS EXERCISE BELOW
+const mainHeading = $("#main-heading").html();
+alert(mainHeading);
 
-// $(".codeup").css("border", "solid 1px red");
+const subArticle = $("#sub-article").html();
+alert(subArticle);
+
+$(".codeup").css("border", "1px solid red");
 
 $("li").css("font-size", "20px");
 
-const myH1 = $("h1").html();
-$("p").html();
-$("li").html();
+$("h1").css("background", "yellow");
+$("p").css("background", "yellow");
+$("li").css("background", "yellow");
 
-alert(myH1);
+alert($(h1).html());
 
 $("h1, p, li");
-
